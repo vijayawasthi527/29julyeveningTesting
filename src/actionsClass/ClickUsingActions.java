6bd73037393ab8e1a744ad277edde4157d789882
vijayawasthi527 <vijayawasthi527@gmail.com>
2024-02-08 16:39:12 +0530

    [vijay]:added

--- src/actionsClass/ClickUsingActions.java
+++ src/actionsClass/ClickUsingActions.java
@@ -6,10 +6,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+
 public class ClickUsingActions {
 
 	public static void main(String[] args) throws InterruptedException
 	{
+		
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com");
--- src/iframe/IframeEg1.java
+++ src/iframe/IframeEg1.java
@@ -1,4 +1,4 @@
-package iframe;
+ackage iframe;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
